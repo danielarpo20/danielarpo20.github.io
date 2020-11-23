5b0f445444f1313702ab4cf72c40b6039ab9b83f
Daniela <danielarpo450@gmail.com>
2020-11-23 17:01:28 -0500

    line down items mobile

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let navBars = document.querySelector("prueba");
+let navOut = document.getElementsByClassName(".");
+let navToggel = document.getElementsByClassName(".");
+
+navBars.addEventListener("click", () => {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.innerHTML = "Close";
+  } else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.innerHTML = "Menu";
+  }
+});
--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-let mainNav = document.getElementsByClassName(".");
-let navToggel = document.getElementsByClassName(".");
-
-navToggel.addEventListener("click", () => {
-  mainNav.clasList.xxx("active");
-});
