5be553541e6b6609a62921784b3d674331da33d7
Daniela <danielarpo450@gmail.com>
2020-11-27 13:49:12 -0500

    navbar active and inactive

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,25 @@
 function darkMode() {
   let element = document.body;
-  let line = document.getElementById('line')
+  let line = document.getElementById("line");
 
   element.classList.toggle("dark-mode");
 
-  console.log(element)
-  console.log(line)
-  line.classList.toggle('dark-mode-line')
+  console.log(element);
+  console.log(line);
+  line.classList.toggle("dark-mode-line");
+}
+
+// Navbar 
+function activeMenu() {
+  document.getElementById("icon");
+  document.querySelector(".navbar").classList.add("active");
+  document.querySelector(".header-icon").classList.add("active");
+  document.querySelector(".header-iconX").classList.add("active");
+}
+
+function inactiveMenu() {
+  document.getElementById("iconX");
+  document.querySelector(".navbar").classList.remove("active");
+  document.querySelector(".header-icon").classList.remove("active");
+  document.querySelector(".header-iconX").classList.remove("active");
 }
