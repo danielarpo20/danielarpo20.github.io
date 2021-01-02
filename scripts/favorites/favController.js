6c28a8f46103ba4f167b9c8a593d206126cfd1d6
Daniela <danielarpo450@gmail.com>
2021-01-01 23:00:17 -0500

    no content favorites section

--- scripts/favorites/favController.js
+++ scripts/favorites/favController.js
@@ -4,10 +4,14 @@ export const favorites = async () => {
   const favoritesSection = document.querySelector(".favorites");
   const principalSection = document.querySelector(".principal");
   const gifosSection = document.querySelector(".myGifos");
-  const createSection = document.querySelector('.createGIF')
+  const createSection = document.querySelector(".createGIF");
+  const buttomSeeMore = document.querySelector(".button");
+  const galeryContainer = document.querySelector(".gifGalery-container");
   favoritesSection.style.display = "initial";
   principalSection.style.display = "none";
   gifosSection.style.display = "none";
   createSection.style.display = "none";
+  buttomSeeMore.style.display = "none";
+  galeryContainer.style.display = "none";
   inactive();
 };
